2087adf9cc4ee6d8625bd74cb9204a199c84c4e5
Hamza Altamas <132405760+HamzaWebPro@users.noreply.github.com>
2025-05-11 01:46:48 +0600

    Update page.js

--- app/&kkQSgDbHWshumdrNcMJaJtDL$wtX3nDJ4NTDg/page.js
+++ app/&kkQSgDbHWshumdrNcMJaJtDL$wtX3nDJ4NTDg/page.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const CORRECT_PIN = "&kkQS+gD+b!HWs*(%humdrNcMJaJtDL+$wtX+3nDJ4^NTD@g";
+const CORRECT_PIN = "Enhance12345";
 const STORAGE_KEY = "my_secure_key";
 
 export default function Login() {
